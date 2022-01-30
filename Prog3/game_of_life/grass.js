d4ccbadbf6d36f0431be0c7ce75f0997fe5a2e91
Meruzhan <85682530+MeruzhanKhach@users.noreply.github.com>
2022-01-30 12:58:01 +0400

    Add files via upload

---
+++ Prog3/game_of_life/grass.js
@@ -0,0 +1,24 @@
+
+
+class Grass extends LivingCreature{
+   
+    constructor(x,y){
+        super(x,y);
+        this.multiplay = 0;
+
+    }
+    mul() {
+        this.multiplay++;
+        let found = this.chooseCell(0);
+        let exact = random(found);
+        if (exact && this.multiplay > 10) {
+           
+            let x = exact[0];
+            let y = exact[1];
+            let grass = new Grass(x, y)
+            matrix[y][x] = 1;
+            grassArr.push(grass);
+            this.multiplay = 0;
+        }
+    }
+}
